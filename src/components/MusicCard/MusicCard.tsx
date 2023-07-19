02f0daa58478cf5dc489d75ff1a3408b1d5dd39f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-07-19 13:59:34 -0300

    Requisito 05, logica do favoritos

--- src/components/MusicCard/MusicCard.tsx
+++ src/components/MusicCard/MusicCard.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
 import { SongType } from '../../types';
+
+import heartEmpty from '../../images/empty_heart.png';
+import heartFilled from '../../images/checked_heart.png';
 
 import './MusicCard.css';
 
@@ -8,6 +12,16 @@ type MusicCardProps = {
 
 function MusicCard(props: MusicCardProps) {
   const { musicList } = props;
+
+  const [favorites, setFavorites] = useState<number[]>([]);
+
+  const handleChange = (musicId: number) => {
+    if (favorites.includes(musicId)) {
+      setFavorites(favorites.filter((id) => id !== musicId));
+    } else {
+      setFavorites([...favorites, musicId]);
+    }
+  };
 
   return (
     <ul className="music-list">
@@ -23,6 +37,29 @@ function MusicCard(props: MusicCardProps) {
             >
               <track kind="captions" />
             </audio>
+            <label
+              className="label-favorite"
+              data-testid={ `checkbox-music-${album.trackId}` }
+              htmlFor={ `favorite-checkbox-${album.trackId}` }
+            >
+              <input
+                id={ `favorite-checkbox-${album.trackId}` }
+                className="favorite-checkbox"
+                type="checkbox"
+                checked={ favorites.includes(album.trackId) }
+                onChange={ () => handleChange(album.trackId) }
+              />
+              <img
+                className="heart-icon"
+                src={ favorites.includes(album.trackId)
+                  ? heartFilled : heartEmpty }
+                alt="favorite"
+              />
+              {/* <span
+                className={ `heart-icon ${favorites.includes(album.trackId)
+                  ? 'filled' : 'empty'}` }
+              /> */}
+            </label>
           </li>
         );
       })}
